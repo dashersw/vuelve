05907efdc2557a4c294d8ed13525a471e3e366da
ibrahim halil sakli <63147096+softwareVirus@users.noreply.github.com>
2024-04-12 23:45:49 +0300

    Refactor the code to implement this keyword type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import {
@@ -16,11 +17,11 @@ import {
   onActivated,
   onDeactivated,
   onServerPrefetch,
-  ComputedGetter,
   WatchCallback,
   WatchSource,
   WatchEffect,
   ComputedRef,
+  UnwrapRef,
   Ref,
 } from 'vue'
 import cloneDeep from 'lodash.clonedeep'
@@ -38,62 +39,74 @@ const vue3LifecycleHooks = {
   deactivated: onDeactivated,
   serverPrefetch: onServerPrefetch,
 }
+type IndexableMethods = Record<string, (...methodArgs: any[]) => any>
 
-export type Composable = {
-  [K in keyof typeof vue3LifecycleHooks]?: (this: Composable, ...args: any[]) => any
+export type Composable<P extends object, D extends object, M extends IndexableMethods, C extends object> = {
+  [K in keyof typeof vue3LifecycleHooks]?: (...args: any[]) => any
 } & {
-  props?: string[]
-  data?: Record<string, unknown>
-  methods?: Record<string, (...args: any[]) => any>
-  computed?: Record<string, ComputedGetter<unknown>>
+  props?: Array<keyof P>
+  data?: D
+  methods?: M
+  computed?: C
   watch?: Record<string, WatchCallback<readonly (object | WatchSource<unknown>)[]>>
   watchEffect?: Record<string, WatchEffect>
-}
+} & ThisType<
+    P &
+      M &
+      {
+        [K in keyof D]: Ref<UnwrapRef<D[K]>>
+      } &
+      { [K in keyof C]: ComputedRef<C[K]> }
+  >
 
-type ComposableReturn<T extends Composable> = {
-  [P in keyof T['data']]?: Ref<T['data'][P]>
-} &
+export default function vuelve<P extends object, D extends object, M extends IndexableMethods, C extends object>(
+  composable: Composable<P, D, M, C>
+): (
+  ...args: any[]
+) => P &
   {
-    [P in keyof T['methods']]: T['methods'][P]
+    [K in keyof D]: Ref<UnwrapRef<D[K]>>
   } &
-  {
-    [P in keyof T['computed']]: ComputedRef<(...args: any[]) => any>
-  }
-
-export default function vuelve<T extends Composable>(composable: T): (...args: any[]) => ComposableReturn<T> {
+  M &
+  { [K in keyof C]: ComputedRef<C[K]> } {
   return function setup(...args: any[]) {
-    const variables = {} as Record<string, unknown>
-    const methods = {} as Record<string, () => unknown>
-    const computeds = {} as Record<string, ComputedRef<unknown>>
+    const props: P = {} as P
+    const data: Partial<{ [K in keyof D]: Ref<UnwrapRef<D[K]>> }> = {}
+    const methods: M = {} as M
+    const computeds: Partial<{ [K in keyof C]: ComputedRef<C[K]> }> = {}
 
-    const context = {}
+    const context = {} as P &
+      {
+        [K in keyof D]: Ref<UnwrapRef<D[K]>>
+      } &
+      M &
+      { [K in keyof C]: ComputedRef<C[K]> }
 
     args.forEach((arg, i) => {
       const propName = composable.props?.[i]
 
-      if (propName) {
-        variables[propName] = arg
+      if (typeof propName === 'string') {
+        props[propName as keyof P] = arg
       }
     })
-
+    Object.assign(context, props)
     if (composable.data) {
       Object.entries(composable.data).forEach(([key, value]) => {
-        variables[key] = ref(cloneDeep(value))
+        data[key as keyof D] = ref(cloneDeep(value)) as Ref<UnwrapRef<D[keyof D]>>
       })
     }
-
-    Object.assign(context, variables)
-
+    
+    Object.assign(context, data)
     if (composable.methods) {
       Object.entries(composable.methods).forEach(([key, value]) => {
-        methods[key] = (...methodArgs: any[]) => value.apply(context, methodArgs)
+        methods[key as keyof M] = ((...methodArgs: any[]) =>
+          (value as (...methodArgs: any[]) => any).apply(context, methodArgs)) as M[keyof M]
       })
     }
 
     Object.assign(context, methods)
-
     Object.entries(vue3LifecycleHooks).forEach(([lifecycleHookName, vue3LifecycleHook]) => {
-      const lifecycleHookNameKey = lifecycleHookName as keyof Composable
+      const lifecycleHookNameKey = lifecycleHookName as keyof Composable<P, D, M, C>
 
       if (composable[lifecycleHookNameKey]) {
         const composableFunction = composable[lifecycleHookNameKey] as Function
@@ -103,9 +116,9 @@ export default function vuelve<T extends Composable>(composable: T): (...args: a
 
     if (composable.watch) {
       Object.entries(composable.watch).forEach(([key, value]) => {
-        if (variables[key]) {
-          watch(variables[key] as readonly (object | WatchSource<unknown>)[], value.bind(context))
-        }
+        if (data[key as keyof D]) {
+          watch(data[key as keyof D] as any, value)
+        } 
       })
     }
 
@@ -117,16 +130,13 @@ export default function vuelve<T extends Composable>(composable: T): (...args: a
 
     if (composable.computed) {
       Object.keys(composable.computed).forEach(key => {
-        const composableComputedFunction = composable.computed?.[key]
+        const composableComputedFunction = composable.computed?.[key as keyof C]
         if (composableComputedFunction) {
-          computeds[key] = computed(composableComputedFunction.bind(context))
+          computeds[key as keyof C] = computed((composableComputedFunction as () => any).bind(context))
         }
       })
     }
-    return {
-      ...variables,
-      ...methods,
-      ...computeds,
-    } as ComposableReturn<T>
+    Object.assign(context, computeds)
+    return context
   }
 }

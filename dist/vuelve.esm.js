64c84ced49a40b6b062d87b9ecca4bd782483e2c
yunus emre kara <yunusemrejs@outlook.com>
2024-04-14 14:31:20 +0300

    Update dist folder with old build version

--- dist/vuelve.esm.js
+++ dist/vuelve.esm.js
@@ -1,86 +1,50 @@
-import { onMounted as E, onBeforeUpdate as y, onUpdated as v, onBeforeUnmount as w, onUnmounted as j, onErrorCaptured as O, onRenderTracked as m, onRenderTriggered as k, onActivated as U, onDeactivated as C, onServerPrefetch as F, ref as T, watch as A, watchEffect as P, computed as x } from "vue";
-import D from "lodash.clonedeep";
-const K = (t) => typeof t == "function", { isArray: $ } = Array, l = {
+import { ref as h, watch as s, watchEffect as p, computed as v, onMounted as E, onBeforeUpdate as m, onUpdated as j, onBeforeUnmount as O, onUnmounted as g, onErrorCaptured as w, onRenderTracked as U, onRenderTriggered as y, onActivated as T, onDeactivated as b, onServerPrefetch as k } from "vue";
+import C from "lodash.clonedeep";
+const x = {
   mounted: E,
-  beforeUpdate: y,
-  updated: v,
-  beforeUnmount: w,
-  unmounted: j,
-  errorCaptured: O,
-  renderTracked: m,
-  renderTriggered: k,
-  activated: U,
-  deactivated: C,
-  serverPrefetch: F
+  beforeUpdate: m,
+  updated: j,
+  beforeUnmount: O,
+  unmounted: g,
+  errorCaptured: w,
+  renderTracked: U,
+  renderTriggered: y,
+  activated: T,
+  deactivated: b,
+  serverPrefetch: k
 };
-function R(t) {
-  return function(...p) {
-    let f = {}, u = {}, s = {}, h = {};
-    const c = {};
-    if (t.props) {
-      const e = $(t.props), n = e ? t.props : Object.keys(t.props), o = (r) => Object.values(t.props)[r];
-      p == null || p.forEach((r, i) => {
-        if (e) {
-          const d = t.props[i];
-          d && (f = {
-            ...f,
-            [d]: r
-          });
-        } else {
-          const d = n[i], a = o(i);
-          if (d && a) {
-            if (r.constructor.name !== a.name)
-              throw new TypeError(
-                `Invalid prop: type check failed for prop "${d}". Expected ${a == null ? void 0 : a.name}, got ${r.constructor.name}`
-              );
-            f = {
-              ...f,
-              [d]: r
-            };
-          }
-        }
-      });
-    }
-    if (t.data && K(t.data)) {
-      const e = t.data();
-      Object.entries(e).forEach(([n, o]) => {
-        const r = T(D(o));
-        u = {
-          ...u,
-          [n]: r
-        };
-      });
-    }
-    return Object.assign(c, u, f), t.methods && Object.entries(t.methods).forEach(([e, n]) => {
-      s = {
-        ...s,
-        [e]: n.bind(c)
-      };
-    }), Object.assign(c, s), Object.entries(l).forEach(([e, n]) => {
-      const o = e;
-      if (t[o]) {
-        const r = t[o];
-        n((...i) => r.apply(c, i));
+function H(t) {
+  return function(...a) {
+    const o = {}, f = {}, i = {}, c = {};
+    return a.forEach((e, n) => {
+      var d;
+      const r = (d = t.props) == null ? void 0 : d[n];
+      r && (o[r] = e);
+    }), t.data && Object.entries(t.data).forEach(([e, n]) => {
+      o[e] = h(C(n));
+    }), Object.assign(c, o), t.methods && Object.entries(t.methods).forEach(([e, n]) => {
+      f[e] = (...r) => n.apply(c, r);
+    }), Object.assign(c, f), Object.entries(x).forEach(([e, n]) => {
+      const r = e;
+      if (t[r]) {
+        const d = t[r];
+        n((...u) => d.apply(c, u));
       }
     }), t.watch && Object.entries(t.watch).forEach(([e, n]) => {
-      const o = n, r = c[e];
-      r && A(r, o.bind(c));
+      o[e] && s(o[e], n.bind(c));
     }), t.watchEffect && Object.values(t.watchEffect).forEach((e) => {
-      P(e.bind(c));
+      p(e.bind(c));
     }), t.computed && Object.keys(t.computed).forEach((e) => {
-      const n = t.computed && t.computed[e];
-      n && (h = {
-        ...h,
-        [e]: x(n.bind(c))
-      });
+      var r;
+      const n = (r = t.computed) == null ? void 0 : r[e];
+      n && (i[e] = v(n.bind(c)));
     }), {
+      ...o,
       ...f,
-      ...u,
-      ...s,
-      ...h
+      ...i
     };
   };
 }
 export {
-  R as default
+  H as default
 };
